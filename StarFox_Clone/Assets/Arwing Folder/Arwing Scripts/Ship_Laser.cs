c2474b3e06c77a67d5703ebe0ebc0fe56c62f4f6
Impact19 <76562437+Impact19@users.noreply.github.com>
2025-01-28 20:16:29 -0500

    Laser Spawn when pressing shoot button
    But the Lasers don't shoot one at a time or become destroyed when in contact with anything.

--- StarFox_Clone/Assets/Arwing Folder/Arwing Scripts/Ship_Laser.cs
+++ StarFox_Clone/Assets/Arwing Folder/Arwing Scripts/Ship_Laser.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Ship_Laser : MonoBehaviour
 {
-    [SerializeField] private GameObject[] lasers;
+    [SerializeField] private GameObject[] laserPool;
+    [SerializeField] private GameObject laserObject;
     [SerializeField] private int laserAmount;
     [SerializeField] private Game_Input gameInput;
+    private int latestLaser, readyLaser;
+
+    private void Awake()
+    {
+        gameInput = new Game_Input();
+    }
     void Start()
     {
-        
+        spawnLaserPool();
+  
     }
 
     // Update is called once per frame
@@ -18,22 +26,40 @@ public class Ship_Laser : MonoBehaviour
         
     }
 
-    private void laserPool() { 
+    private void getLaser() {
 
+        for (int x = 0; x < laserPool.Length; x++) {
+            laserPool[x].SetActive(true);
+        }
+        
+    }
+
+    private void spawnLaserPool() {
+        laserPool = new GameObject[laserAmount]; 
+        for (int x = 0; x < laserAmount; x++) {
+            laserPool[x] = Instantiate(laserObject, gameObject.transform);
+            laserPool[x].SetActive(false); 
+        }
     }
 
     private void shootLaser(InputAction.CallbackContext context) {
-        Debug.Log("Shoot Laser"); 
+        Debug.Log("Shoot Laser");
+        getLaser(); 
+
     }
 
     private void OnEnable()
     {
+        gameInput.Ship.Shoot.Enable();
         gameInput.Ship.Shoot.performed += shootLaser;
         gameInput.Ship.Shoot.canceled += shootLaser;
+
+
     }
 
     private void OnDisable()
     {
+        gameInput.Ship.Shoot.Disable(); 
         gameInput.Ship.Shoot.performed -= shootLaser;
         gameInput.Ship.Shoot.performed -= shootLaser;
     }

d1e26b00461e859f2b98135cae250fc52154b347
Impact19 <76562437+Impact19@users.noreply.github.com>
2025-04-21 21:10:43 -0400

    Added SmoothDamp for Camera Control

--- StarFox_Clone/Assets/Camera_Management.cs
+++ StarFox_Clone/Assets/Camera_Management.cs
@@ -8,19 +8,22 @@ public class Camera_Management : MonoBehaviour
     [SerializeField] private Ship_Rail_Movement railMovement;
     private Rigidbody cameraRB;
     [SerializeField] private Vector3 distanceFromShip;
-    [SerializeField] private GameObject ship; 
+    [SerializeField] private GameObject ship;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothTime;  
+    private Vector3 velocity = Vector3.zero;
     void Start()
     {
         cameraRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
 
-       cameraRB.position =  Vector3.MoveTowards(cameraRB.position, ship.transform.position - distanceFromShip, Time.deltaTime * railMovement.getShipRailSpeed() );
-
+      // cameraRB.position =  Vector3.MoveTowards(cameraRB.position, ship.transform.position - distanceFromShip, Time.deltaTime * railMovement.getShipRailSpeed() );
+        transform.position = Vector3.SmoothDamp(transform.position, ship.transform.position, ref velocity, smoothTime);
     } 
 
 

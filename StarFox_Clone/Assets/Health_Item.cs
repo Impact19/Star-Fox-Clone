49ac698207092293a44b31f4af445b60a3c235f9
Impact19 <76562437+Impact19@users.noreply.github.com>
2026-01-16 14:22:34 -0500

    Health Object now floats up and down
    Used a script for this instead of an animation and it's easier to control to is choose this one.

--- StarFox_Clone/Assets/Health_Item.cs
+++ StarFox_Clone/Assets/Health_Item.cs
@@ -7,14 +7,21 @@ public class Health_Item : MonoBehaviour
     [SerializeField] private string playerTag;
     [SerializeField] private float healthAmount;
     [SerializeField] private AudioClip healClip;
-    private AudioSource audioS; 
-    // Start is called before the first frame update
+    private AudioSource audioS;
+    [SerializeField] private float floatSpeed;
+    [SerializeField] private float floatHeight;
+    private Vector3 startPosition; 
     void Start()
     {
         audioS = GetComponent<AudioSource>();
-        audioS.clip = healClip; 
+        audioS.clip = healClip;
+        startPosition = transform.position; 
     }
 
+    private void Update()
+    {
+        floatItem(); 
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == playerTag) {
@@ -26,7 +33,13 @@ public class Health_Item : MonoBehaviour
 
     public float getHealing() {
         return healthAmount;
-    } 
+    }
+
+    private void floatItem() {
+        float changeY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(transform.position.x, changeY, transform.position.z); 
+    }
+
 
 
 }

def4211b2b0cb97f5d5f988ef39d316f9f09a676
Impact19 <76562437+Impact19@users.noreply.github.com>
2026-07-15 22:30:53 -0400

    Bomb now works denoates correctly
    However it still doesn't denonate with Bomb's Lifetime ends.

--- StarFox_Clone/Assets/Arwing Folder/Arwing Scripts/Ship_Bombs.cs
+++ StarFox_Clone/Assets/Arwing Folder/Arwing Scripts/Ship_Bombs.cs
@@ -11,18 +11,14 @@ public class Ship_Bombs : MonoBehaviour
     [SerializeField] private Projectile_Properties bomb, bombExplo;
     [SerializeField] private float bombAmount, bombMax;
     [SerializeField] private TMP_Text bombAmountUI; 
-    [SerializeField] private bool isBombFired;
     [SerializeField] private string bombItemTag; 
     [SerializeField] private Transform shipTurret;
-    private float buttonCount; 
+     
     
     void Start()
     {
-        isBombFired = false;
-        buttonCount = 0; 
-        bombExplo.gameObject.SetActive(false);
-        bomb.gameObject.SetActive(false);
-     
+       
+        
     }
 
     // Update is called once per frame
@@ -34,22 +30,24 @@ public class Ship_Bombs : MonoBehaviour
 
     private void FixedUpdate()
     { 
-        if(!isBombFired) bomb.transform.position = shipTurret.position;
+        if(!bomb.gameObject.activeSelf) bomb.transform.position = shipTurret.position;
     }
     private void Awake()
     {
         gameInput = new Game_Inputs();
     }
     private void fireBomb(InputAction.CallbackContext context) {
-         
-            if (!isBombFired && !bombExplo.gameObject.activeSelf && bombAmount > 0)
+        bool isBombActive = bomb.gameObject.activeSelf;
+        bool isExploActive = bombExplo.gameObject.activeSelf;  
+
+            if (!isBombActive && !isExploActive && bombAmount > 0)
             {
                 Debug.Log("Shot Bomb");
                 bombAmount--;
                 bomb.gameObject.SetActive(true); 
-                isBombFired = true;
+              
             }
-            else if(isBombFired && bomb.gameObject.activeSelf)
+            else if(isBombActive && !isExploActive)
             {
                 exploBomb(); 
             }
@@ -61,7 +59,6 @@ public class Ship_Bombs : MonoBehaviour
         bombExplo.transform.position = bomb.transform.position;
         bombExplo.gameObject.SetActive(true);
         bomb.removeProjectile();
-        isBombFired = false;
     }
 
     public void increaseBomb(float bombs) { 
